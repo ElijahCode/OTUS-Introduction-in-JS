aed9f12dc251aae4d1ffc3f3abaa7a810bca6262
Ilya <ilyastarickov1996@gmail.com>
2021-01-29 20:56:12 +0400

    Wrote tests for HM#3. Rewrite code in HM#3 for more comfortable testing.

--- HomeWork№3/script.js
+++ HomeWork№3/script.js
@@ -1,40 +1,57 @@
 //exercise #1
 
-let sum = 0;
+export function getSumOfNumbers (firstNumb, lastNumb) {
+	let sum = 0;
 
-for (let i = 50; i < 101; i++) {
-	sum = sum + i;
+	for (let i = firstNumb; i < lastNumb + 1; i++) {
+		sum = sum + i;
+	}
+
+	return sum;
 }
 
+
+
 console.log('Result for exercise #1');
-console.log(`${sum}`);
+console.log(getSumOfNumbers(50, 100));
 console.log('');
 
 //exercise #2
 
 console.log('Result for exercise #2');
 
-for (let i = 1; i < 11; i++) {
-	console.log(7 * i);
+export function getTableOfMultiplication(number, sizeOfTable) {
+	let result = [];
+
+	for (let i = 1; i < sizeOfTable + 1; i++) {
+		result.push(number * i);
+	}
+
+	return result;
 }
 
+console.log(getTableOfMultiplication(7, 10));
 console.log('');
 
 //exercise #3
 
-let n = prompt(`Enter a number:`);
-let result = 0;
-let counter = 0;
+export function getSumOfOddNumbers (number) {
+	
+	let result = 0;
+	let counter = 0;
 
-for(let i = 1; i < n; i++) {
-	if(i % 2 > 0) {
-		result = result + i;
-		counter++;
+	for(let i = 1; i < number + 1; i++) {
+		if(i % 2 > 0) {
+			result = result + i;
+			counter++;
+		}
 	}
+	
+	result = result / counter;
+
+	return result;
 }
 
-result = result / counter;
-
 console.log('Result for exercise #3');
-console.log(`${result}`);
+console.log(getSumOfOddNumbers(10));
 console.log('');
