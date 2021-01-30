56d126f4b463a27f1fe5d3a34268ae4f9f7f127b
Ilya <ilyastarickov1996@gmail.com>
2021-01-30 15:19:17 +0400

    Fixed work script.js in HM№3

--- HomeWork№3/script.js
+++ HomeWork№3/script.js
@@ -3,7 +3,7 @@
 export function getSumOfNumbers(firstNumb, lastNumb) {
   let sum = 0;
 
-  for (let i = firstNumb; i < lastNumb + 1; i += i) {
+  for (let i = firstNumb; i < lastNumb + 1; i += 1) {
     sum += i;
   }
 
@@ -21,7 +21,7 @@ console.log("Result for exercise #2");
 export function getTableOfMultiplication(number, sizeOfTable) {
   const result = [];
 
-  for (let i = 1; i < sizeOfTable + 1; i += i) {
+  for (let i = 1; i < sizeOfTable + 1; i += 1) {
     result.push(number * i);
   }
 
@@ -37,10 +37,10 @@ export function getSumOfOddNumbers(number) {
   let result = 0;
   let counter = 0;
 
-  for (let i = 1; i < number + 1; i += i) {
+  for (let i = 1; i < number + 1; i += 1) {
     if (i % 2 > 0) {
       result += i;
-      counter += counter;
+      counter += 1;
     }
   }
 
