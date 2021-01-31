c31fb168d1f8bbbee6dfd3f0b0596a00d6cb1a2e
Ilya <ilyastarickov1996@gmail.com>
2021-01-31 14:55:36 +0400

    Wrote tests for HM#9. Rewrite code in HM#9 for more comfortable testing.

--- HomeWork№9/script.js
+++ HomeWork№9/script.js
@@ -1,6 +1,6 @@
 // exercise #1
 
-function isRightTriangle(a, b, c) {
+export function isRightTriangle(a, b, c) {
   const check1 = a ** 2 === b ** 2 + c ** 2;
   const check2 = b ** 2 === a ** 2 + c ** 2;
   const check3 = c ** 2 === b ** 2 + a ** 2;
@@ -20,53 +20,65 @@ console.log("");
 
 // exercise #2
 
-function calculateCircle(R) {
+export function calculateCircle(R) {
   const l = 2 * Math.PI * R;
   const S = Math.PI * R ** 2;
 
-  console.log(
-    `Square of circle: ${S.toFixed(3)}, circumference : ${l.toFixed(3)}`
-  );
-  return null;
+  return [S, l];
 }
 
 console.log("Result for exercise #2:");
-calculateCircle(prompt(`Enter a radius of circle:`, "2"));
+const data = calculateCircle(prompt(`Enter a radius of circle:`, "2"));
+console.log(
+  `Square of circle: ${data[0].toFixed(
+    3
+  )}, circumference of circle : ${data[1].toFixed(3)}`
+);
 console.log("");
 
 // exercise #3
 
-function findRoots(str) {
-  const [a, b, c] = str.split(" ");
+export function findRoots(str) {
+  let a;
+  let b;
+  let c;
+  const defaultStr = "5 15 4";
+  try {
+    [a, b, c] = str.split(" ");
+  } catch (err) {
+    [a, b, c] = defaultStr.split(" ");
+  }
 
   const D = b * b - 4 * a * c;
 
   let x1;
   let x2;
+  const result = [];
 
   if (D < 0) {
     console.log("Discrimant < 0");
 
-    x1 = -b / (2 * a) + Math.sqrt(Math.abs(D)) / (2 * a);
-    x2 = -b / (2 * a) - Math.sqrt(Math.abs(D)) / (2 * a);
+    x1 = `${-b / (2 * a)} + ${Math.sqrt(Math.abs(D)) / (2 * a)} * i`;
+    x2 = `${-b / (2 * a)} - ${Math.sqrt(Math.abs(D)) / (2 * a)} * i`;
 
-    console.log(`For eqution ${a}x^2 + ${b}x + c = 0 have next complex roots:`);
-    console.log(`x1 = ${x1.toFixed(3)} * i, x2 = ${x2.toFixed(3)} * i`);
+    result.push(x2, x1);
   }
   if (D === 0) {
-    x1 = (-b / 2) * a;
-    x2 = x1;
+    x1 = -b / (2 * a);
+
+    result.push(x1);
   } else {
     x1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a);
     x2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a);
+
+    result.push(x2, x1);
   }
 
-  console.log(`For eqution ${a}x^2 + ${b}x + c = 0 have next roots:`);
-  console.log(`x1 = ${x1.toFixed(3)}, x2 = ${x2.toFixed(3)}`);
-
-  return null;
+  return result;
 }
 
 console.log("Result for exercise #3:");
-findRoots(prompt("Enter coefficients a b c separated by a space:", "5 15 4"));
+console.log(
+  findRoots(prompt("Enter coefficients a b c separated by a space:", "5 15 4"))
+);
 console.log("");
