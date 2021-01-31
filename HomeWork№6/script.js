f6a9936f0e12f4308d73288f1695a6cd18941b20
Ilya <ilyastarickov1996@gmail.com>
2021-01-31 10:39:23 +0400

    Wrote tests for HM#6. Rewrite code in HM#6 for more comfortable testing.

--- HomeWork№6/script.js
+++ HomeWork№6/script.js
@@ -1,6 +1,6 @@
 // exercise #1
 
-function diff(a, b) {
+export function diff(a, b) {
   return a > b ? a - b : b - a;
 }
 
@@ -10,7 +10,7 @@ console.log("");
 
 // exercise #2
 
-function isWord(str) {
+export function isWord(str) {
   return str.split(" ").length < 2;
 }
 
@@ -24,9 +24,9 @@ console.log("");
 
 // exercise #3
 
-function pow(a, x) {
+export function pow(a, x) {
   let result = a;
-  for (let i = 1; i < x; i += i) {
+  for (let i = 1; i < x; i += 1) {
     result *= a;
   }
   return result;
