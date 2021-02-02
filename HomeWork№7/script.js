6ccbecc9cec9ee71c78e6f001deb747dffd38093
Ilya <ilyastarickov1996@gmail.com>
2021-02-02 20:42:22 +0400

    Optimize code for work with jest.

--- HomeWork№7/script.js
+++ HomeWork№7/script.js
@@ -1,31 +1,64 @@
-const button = document.querySelector(".button");
+function createButton(elem) {
+  const button = document.createElement("button");
+
+  button.innerHTML = `<button class = "button" onclick = 'buttonClick()'>
+    Click!
+  </button>`;
+
+  elem.append(button);
+}
+
+function createInput(elem) {
+  const input = document.createElement("input");
+
+  input.type = "text";
+  input.id = "textbox";
+
+  elem.append(input);
+}
+
+function createMain(elem) {
+  const main = document.createElement("main");
+  main.id = "main";
+
+  elem.append(main);
+}
 
 function getInputText() {
-  const text = document.querySelector(".textbox");
-  return text.value;
+  const text = document.getElementById("textbox").value;
+  return text;
 }
 
 function addParagraph(data) {
   const paragraphElem = document.createElement("p");
   paragraphElem.innerText = data;
-  const main = document.querySelector(".main");
+  const main = document.getElementById("main");
   main.append(paragraphElem);
 
   if (main.getElementsByTagName("p").length > 5) {
-    console.log(main.getElementsByTagName("p").item(0));
     main.removeChild(main.getElementsByTagName("p").item(0));
   }
 }
 
 export function buttonClick() {
   const inputText = getInputText();
+
   addParagraph(inputText);
 }
 
+createMain(document.querySelector("body"));
+addParagraph("Test text");
+addParagraph("Test text");
+addParagraph("Test text");
+createInput(document.querySelector("body"));
+createButton(document.querySelector("body"));
+
+const button = document.querySelector(".button");
+
 function hideButton() {
-  const text = document.querySelector(".textbox");
   setInterval(() => {
-    if (text.value === "") {
+    const text = document.getElementById("textbox").value;
+    if (text === "") {
       button.hidden = true;
     } else {
       button.hidden = false;
